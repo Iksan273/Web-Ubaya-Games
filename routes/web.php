4cc711c336c567956a12e664506ffab0f7a6964d
Muhammad Ikhsan <iksan8991@gmail.com>
2024-03-23 22:25:53 +0700

    update git all file

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\VoliController;
 //--AUTH ROUTING--//
 Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'registerForm'])->name('register.form');
+// Route::get('/register', [AuthController::class, 'registerForm'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
